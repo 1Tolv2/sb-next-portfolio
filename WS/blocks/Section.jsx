6facca7de0a222e5eeff801bd18142722920cf09
sofia johnsson <sofia.johnsson@willandskill.se>
2022-12-14 11:29:31 +0100

    Added max width to sections

--- WS/blocks/Section.jsx
+++ WS/blocks/Section.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import { storyblokEditable } from "@storyblok/react";
 
 const Section = ({ blok, children }) => {
-  console.log("SECTION", blok);
   return (
     <div
       id={blok?.section_name_id}
-      className={`-scroll-mt-1 bg-${
-        blok?.background_color
-      } w-screen min-h-screen py-10 px-4 md:px-10 lg:p-32 flex flex-col-reverse md:flex-row justify-center ${
-        blok?.center_content && "items-center"
-      }`}
-      {...storyblokEditable(blok)}
+      className={`-scroll-mt-1 bg-${blok?.background_color}`}
     >
-      {children}
+      <div
+        className={`max-w-[1920px] w-screen min-h-screen py-10 px-4 md:px-10 lg:p-32 flex flex-col-reverse md:flex-row justify-center ${
+          blok?.center_content && "items-center"
+        }`}
+        {...storyblokEditable(blok)}
+      >
+        {children}
+      </div>
     </div>
   );
 };
