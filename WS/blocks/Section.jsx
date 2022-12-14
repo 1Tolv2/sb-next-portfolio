1fb01fdee1adf3773234f5ce5a48028ee594be4b
sofia johnsson <sofia.johnsson@willandskill.se>
2022-12-14 14:55:44 +0100

    fixed centering when largest scale

--- WS/blocks/Section.jsx
+++ WS/blocks/Section.jsx
@@ -8,7 +8,7 @@ const Section = ({ blok, children }) => {
       className={`-scroll-mt-1 bg-${blok?.background_color}`}
     >
       <div
-        className={`max-w-[1920px] w-screen min-h-screen py-10 px-4 md:px-10 lg:p-32 flex flex-col-reverse md:flex-row justify-center ${
+        className={`max-w-[1920px] mx-auto w-screen min-h-screen py-10 px-4 md:px-10 lg:p-32 flex flex-col-reverse md:flex-row justify-center ${
           blok?.center_content && "items-center"
         }`}
         {...storyblokEditable(blok)}
