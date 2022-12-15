07a2e6c22179928516c24973d2ddd35c7d85ee60
sofia johnsson <sofia.johnsson@willandskill.se>
2022-12-15 17:20:03 +0100

    added new components

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import { storyblokInit, apiPlugin } from "@storyblok/react";
 import "../styles/globals.css";
-import ArticlePreviewList from "../WS/block-components/ArticlePreviewList";
 import Hero from "../WS/blocks/Hero";
 import ImageTextSideBySide from "../WS/blocks/ImageTextSideBySide";
-import ProjectPreviewList from "../WS/block-components/ProjectPreviewList";
 import TitledGrid from "../WS/blocks/TitledGrid";
 import Page from "../WS/blocks/Page";
+
+import ArticlePreviewList from "../WS/block-components/ArticlePreviewList";
+import ProjectPreviewList from "../WS/block-components/ProjectPreviewList";
+import Avatar from "../WS/block-components/Avatar";
+import ImageWithBackground from "../WS/block-components/ImageWithBackground";
 
 const components = {
   "article-preview-list": ArticlePreviewList,
@@ -15,6 +18,8 @@ const components = {
   "project-preview-list": ProjectPreviewList,
   "titled-grid": TitledGrid,
   "sub-page": Page,
+  avatar: Avatar,
+  "image-with-background": ImageWithBackground,
 };
 
 storyblokInit({
