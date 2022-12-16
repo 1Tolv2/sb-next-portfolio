192892f847e67e3fd1ac6aa68c97f7021c2c9731
sofia johnsson <sofia.johnsson@willandskill.se>
2022-12-16 15:17:44 +0100

    Now renders actual content

--- WS/block-components/ArticlePreviewList.jsx
+++ WS/block-components/ArticlePreviewList.jsx
@@ -4,28 +4,29 @@ import Text from "../components/Typography/Text";
 import Link from "next/link";
 
 const ArticlePreviewList = ({ blok }) => {
-  const arrayOfThree = ["", "", ""];
+  console.log("BLOK", blok);
   return (
     <div className="grid lg:grid-cols-3 lg:grid-rows-1 w-3/4 lg:w-auto mt-5 md:mt-0 mx-auto -mb-16 gap-x-5 xl:gap-x-14 items-end">
-      {arrayOfThree.map((article) => {
+      {blok?.articles?.map((article) => {
+        const { content } = article;
         return (
           <div className="relative h-fit w-full lg:mt-20">
             <div>
               <img
                 className="w-full"
-                src="clement-helardot-95YRwf6CNw8-unsplash.jpg"
+                src={content?.image?.filename}
+                alt={content?.image?.alt}
               />
             </div>
             <div className="relative bottom-20 w-11/12 mx-auto bg-neutral px-3 lg:px-5 pt-4 lg:pt-11 pb-3 lg:pb-7">
-              <Heading type="h3">CSS hjälpmedel för nybörjare</Heading>
-              <Text>
-                Lorem ipsum dolor sit amet, ei postea iuvaret cum, dicam graecis
-                eos no. Te per populo nonumes. Tollit voluptua eu duo, nec quas
-                aliquando interpretaris eu.
-              </Text>
+              <Heading type="h3">{article?.name}</Heading>
+              <Text>{content?.summary}</Text>
               <div className="flex my-3 ">
-                <Link className="flex items-center h-fit ml-1 mt-5" href="/">
-                  <Text type="emphasize">Read article</Text>
+                <Link
+                  className="flex items-center h-fit ml-1 mt-5"
+                  href={article?.full_slug}
+                >
+                  <Text type="emphasize">{blok?.link_text}</Text>
                   <img
                     className="ml-2 h-4 lg:h-6"
                     src="/external_link_icon.svg"
