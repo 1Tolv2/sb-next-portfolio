84401c975d3fde2cef7f1c2d04876b115b3dd08c
sofia johnsson <sofia.johnsson@willandskill.se>
2022-12-16 15:17:59 +0100

    optional style on button, full width

--- WS/components/Button/Button.jsx
+++ WS/components/Button/Button.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-const Button = ({ type, children, event }) => {
+const Button = ({ type, children, event, fullWidth }) => {
   return type === "ghost" ? (
     <button className="" onClick={event}>
       {children}
     </button>
   ) : (
     <button
-      className="flex justify-center items-center text-neutral bg-primary-main w-fit px-14 py-3 rounded"
+      className={`flex justify-center items-center text-neutral bg-primary-main px-14 py-3 rounded ${
+        fullWidth ? "w-full" : "w-full md:w-fit"
+      }`}
       onClick={event}
     >
       {children}
