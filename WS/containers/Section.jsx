5d4b3e44e96e11af12ca1688b164bed8c4262ce7
sofia johnsson <sofia.johnsson@willandskill.se>
2022-12-16 14:15:19 +0100

    modified scroll margin

--- WS/containers/Section.jsx
+++ WS/containers/Section.jsx
@@ -6,8 +6,8 @@ const Section = ({ blok, children, pageType, isFirstSection }) => {
     <div
       id={blok?.section_name_id}
       className={`${isFirstSection ? "lg:snap-start" : "lg:snap-center"} ${
-        pageType === "sub-page" && "pt-10 md:pt-20"
-      } -scroll-mt-1 bg-${blok?.background_color}`}
+        pageType === "sub-page" && "pt-20 md:pt-20 lg:pt-10"
+      } scroll-mt-28 md:scroll-mt-32 bg-${blok?.background_color}`}
     >
       <div
         className={`max-w-[1920px] mx-auto w-screen min-h-screen py-10 px-4 md:px-10 lg:p-32 flex flex-col-reverse md:flex-row justify-center ${
