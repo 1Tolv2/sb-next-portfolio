1840f118192360c8e8a2924a6f3ae1201fcd18b2
sofia johnsson <sofia.johnsson@willandskill.se>
2022-12-20 08:10:47 +0100

    Added printing capabilities

--- WS/block-components/CvTab.jsx
+++ WS/block-components/CvTab.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Heading from "../components/Typography/Heading";
 import Text from "../components/Typography/Text";
 import { StoryblokComponent } from "@storyblok/react";
+import RichTextRenderer from "../components/RichTextRenderer";
 
 const renderMainContent = (title, content) => {
   return (
@@ -24,42 +25,45 @@ const renderMainContent = (title, content) => {
     </div>
   );
 };
-
+const renderSideContent = (title, blok) => {
+  return (
+    <div className="w-1/3 md:w-auto print:w-full print:h-min">
+      <Heading type="h4">{title}</Heading>
+      {blok.content ? (
+        <RichTextRenderer content={blok} />
+      ) : (
+        <ul className="text-left">
+          {blok?.map((item, index) => {
+            const text = typeof item === "string" ? item : item?.text;
+            return (
+              <li key={item?._uid || index}>
+                {title === "Contact" && <img src="" />}
+                <Text type="emphasize">{text}</Text>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
 const CvTab = ({ blok }) => {
   console.log("BLOK", blok);
   return (
-    <div className="flex flex-wrap flex-cols">
-      <div className="w-1/4 text-center">
-        <div>
-          <Heading type="h4">Contact</Heading>
-          <ul className="text-left">
-            {blok?.contact?.map((item) => {
-              return (
-                <li key={item?._uid}>
-                  <Text type="emphasize">{item.text}</Text>
-                </li>
-              );
-            })}
-          </ul>
+    <div className="flex flex-wrap flex-col-reverse md:flex-row print:p-0 print:w-full print:flex-row print:flex-wrap">
+      <div className="flex gap-10 md:block w-full md:w-1/4 text-center print:gap-0 print:w-1/4 print:flex-wrap">
+        <div className="max-h-[calc(401px-20px)] w-[calc(100%-20px)] mb-[20px] overflow-clip">
+          <img
+            className="h-auto w-full"
+            src="https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="profile image"
+          />
         </div>
-        <div>
-          <Heading type="h4">Skills</Heading>
-          <ul className="text-left">
-            {blok?.skills?.map((item, index) => {
-              return <li key={index}>{item}</li>;
-            })}
-          </ul>
-        </div>
-        <div>
-          <Heading type="h4">Languages</Heading>
-          <ul className="text-left">
-            {blok?.languages?.map((item, index) => {
-              return <li key={index}>{item}</li>;
-            })}
-          </ul>
-        </div>
+        {renderSideContent("Contact", blok?.contact)}
+        {renderSideContent("Skills", blok?.skills)}
+        {renderSideContent("Languages", blok?.languages)}
       </div>
-      <div className="w-3/4 pl-8">
+      <div className="w-full md:w-3/4 pl-8 print:pl-4 print:w-3/4">
         <div className="text-center">
           <Heading>{blok?.name}</Heading>
           <Text center type="emphasizeBig">
--- WS/components/CoverModal.jsx
+++ WS/components/CoverModal.jsx
@@ -8,28 +8,32 @@ const CoverModal = ({ blok, closeModal }) => {
 
   return (
     <div className="fixed z-[100] top-0 left-0 w-screen h-screen">
-      <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 h-fit w-[calc(100vw-20px)] lg:w-[calc(100vw-100px)] bg-white p-24">
-        <div className="absolute top-10 right-10">
-          <Button event={() => console.log("Clicked")}>Download</Button>
+      <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 h-full w-[calc(100vw-20px)] lg:w-[calc(100vw-100px)] bg-white pt-10 pb-32 px-4 lg:p-24">
+        <div className="flex justify-between">
+          <ul className="flex print:hidden">
+            {blok?.map((item) => {
+              return (
+                <li
+                  key={item._uid}
+                  onClick={() => setActiveTab(item?.component)}
+                  className={`mr-5 p-3 cursor-pointer border-primary-light ${
+                    item?.component === activeTab && "border-b-4"
+                  }`}
+                >
+                  <Text capitalized type="emphasizeBig">
+                    {item?.component.replace("-", " ")}
+                  </Text>
+                </li>
+              );
+            })}
+          </ul>
+          <div className="fixed bottom-5 right-5 md:static h-fit shadow-lg print:hidden">
+            <Button fullWidth event={() => window.print()}>
+              Download
+            </Button>
+          </div>
         </div>
-        <ul className="flex">
-          {blok?.map((item) => {
-            return (
-              <li
-                key={item._uid}
-                onClick={() => setActiveTab(item?.component)}
-                className={`mr-5 p-3 cursor-pointer border-primary-light ${
-                  item?.component === activeTab && "border-b-4"
-                }`}
-              >
-                <Text capitalized type="emphasizeBig">
-                  {item?.component.replace("-", " ")}
-                </Text>
-              </li>
-            );
-          })}
-        </ul>
-        <div className="px-3 py-5 max-w-[1920px]">
+        <div className="px-3 py-5 max-w-[1920px] overflow-y-scroll h-full">
           <StoryblokComponent
             blok={blok?.find(
               (nestedBlok) => nestedBlok.component === activeTab
