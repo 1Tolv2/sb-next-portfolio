d590545699d5140624143e6c3c441cb5a2b107ad
sofia johnsson <sofia.johnsson@willandskill.se>
2022-12-20 10:53:46 +0100

    simplified text alignment conditional

--- WS/components/Typography/Text.jsx
+++ WS/components/Typography/Text.jsx
@@ -3,42 +3,36 @@ import React from "react";
 const defaultClasses = "font-sans text-inherit";
 
 const Texts = {
-  body: (children, center, isBackgroundDark) => (
+  body: (children, align, isBackgroundDark) => (
     <p
-      className={`${defaultClasses} my-4 text-${
-        center ? "center" : "left"
-      } text-sm xl:text-xl text-${
+      className={`${defaultClasses} my-4 text-${align} text-sm xl:text-xl text-${
         isBackgroundDark ? "neutral" : "black"
       } print:my-1`}
     >
       {children}
     </p>
   ),
-  emphasize: (children, center, isBackgroundDark) => (
+  emphasize: (children, align, isBackgroundDark) => (
     <p
-      className={`${defaultClasses} text-${
-        center ? "center" : "left"
-      } text-sm xl:text-xl text-${isBackgroundDark ? "neutral" : "black"}`}
+      className={`${defaultClasses} text-${align} text-sm xl:text-xl text-${
+        isBackgroundDark ? "neutral" : "black"
+      }`}
     >
       {children}
     </p>
   ),
-  emphasizeBig: (children, center, isBackgroundDark) => (
+  emphasizeBig: (children, align, isBackgroundDark) => (
     <p
-      className={`${defaultClasses} text-${
-        center ? "center" : "left"
-      } text-xl lg:text-2xl text-${
+      className={`${defaultClasses} text-${align} text-xl lg:text-2xl text-${
         isBackgroundDark ? "neutral" : "primary-main"
       }`}
     >
       {children}
     </p>
   ),
-  tags: (children, center, isBackgroundDark) => (
+  tags: (children, align, isBackgroundDark) => (
     <p
-      className={`${defaultClasses} font-serif mt-1.5 md:my-3 text-${
-        center ? "center" : "left"
-      } text-sm font-semibold lg:text-base text-${
+      className={`${defaultClasses} font-serif mt-1.5 md:my-3 text-${align} text-sm font-semibold lg:text-base text-${
         isBackgroundDark ? "neutral" : "primary-light"
       }`}
     >
@@ -50,11 +44,12 @@ const firstCharacterUpperCase = (string) => {
   return string[0].toUpperCase() + string.substring(1);
 };
 
-const Text = ({ type, children, center, isBackgroundDark, capitalized }) => {
+const Text = ({ type, children, isBackgroundDark, capitalized, align }) => {
+  align = align ? align : "left";
   children = capitalized ? firstCharacterUpperCase(children) : children;
   return type
-    ? Texts[type](children, center, isBackgroundDark)
-    : Texts.body(children, center);
+    ? Texts[type](children, align, isBackgroundDark)
+    : Texts.body(children, align);
 };
 
 export default Text;
