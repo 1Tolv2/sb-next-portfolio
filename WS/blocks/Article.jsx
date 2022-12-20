63e04a834229120a4ab3d003b5f3d579e3ced465
sofia johnsson <sofia.johnsson@willandskill.se>
2022-12-20 15:37:11 +0100

    Styled blogpost

--- WS/blocks/Article.jsx
+++ WS/blocks/Article.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import RichTextRenderer from "../components/RichTextRenderer";
+import Heading from "../components/Typography/Heading";
+import Text from "../components/Typography/Text";
 
 const Article = ({ blok }) => {
-  console.log("BLOK", blok);
-  return <div>Article</div>;
+  return (
+    <div className="bg-neutral min-h-screen">
+      <div className="bg-white max-w-[1000px] py-32 md:py-56 px-6 md:px-20 mx-auto">
+        <img className="w-full mb-10" src={blok?.image?.filename} />
+        <div className="mb-14">
+          <Heading>{blok?.title}</Heading>
+          <Text type="emphasizeBig">{blok?.preamble}</Text>
+          <div></div>
+        </div>
+        <div className="sm:w-2/4 xl:w-3/4 mx-auto">
+          <RichTextRenderer content={blok?.text} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
