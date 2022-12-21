b51c368fd1a9e6dbad0433203c5956b6b71c4e76
sofia johnsson <sofia.johnsson@willandskill.se>
2022-12-21 08:45:03 +0100

    fixed link spacing on cards

--- WS/components/ProjectCards/ProjectMobileCard.jsx
+++ WS/components/ProjectCards/ProjectMobileCard.jsx
@@ -10,8 +10,8 @@ const ProjectMobileCard = ({ story }) => {
     <div className="h-fit w-full" key={story.id}>
       <Heading type="h3">{nestedBlok?.title}</Heading>
       <div className="flex">
-        <div>
-          <div className="relative">
+        <div className="flex flex-col justify-between relative">
+          <div>
             <Text>{nestedBlok?.description}</Text>
             <AccentLine />
             <div className="flex flex-wrap items-center">
@@ -26,30 +26,25 @@ const ProjectMobileCard = ({ story }) => {
                 );
               })}
             </div>
-            <div className="my-3 ">
-              <Link
-                className="flex items-center h-fit mb-1 lg:mb-0"
-                href={nestedBlok?.github_link?.url || "/"}
-              >
-                <Text type="emphasize">Go to demo</Text>
-                <img
-                  className="ml-2 h-4 lg:h-6"
-                  src="/external_link_icon.svg"
-                />
-              </Link>
-              <Link
-                className="flex items-center h-fit w-fit mr-2"
-                href={nestedBlok?.github_link?.url || "/"}
-              >
-                <Text type="emphasize">View source code</Text>
-                <img
-                  className="ml-2 h-4 lg:h-6"
-                  src="/external_link_icon.svg"
-                />
-              </Link>
-            </div>
+          </div>
+          <div className="my-3 ">
+            <Link
+              className="flex items-center h-fit mb-1 lg:mb-0"
+              href={nestedBlok?.github_link?.url || "/"}
+            >
+              <Text type="emphasize">Go to demo</Text>
+              <img className="ml-2 h-4 lg:h-6" src="/external_link_icon.svg" />
+            </Link>
+            <Link
+              className="flex items-center h-fit w-fit mr-2"
+              href={nestedBlok?.github_link?.url || "/"}
+            >
+              <Text type="emphasize">View source code</Text>
+              <img className="ml-2 h-4 lg:h-6" src="/external_link_icon.svg" />
+            </Link>
           </div>
         </div>
+
         <img
           className="w-full self-end"
           src={nestedBlok?.images?.[0]?.filename}
