689778c485309a676f6c38ed94493e0b06046224
sofia johnsson <sofia.johnsson@willandskill.se>
2022-12-21 10:37:50 +0100

    added meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,16 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta charset="UTF-8" />
+          <meta
+            name="description"
+            content="Full stack developer Sofia Johnsson S.'s portfolio"
+          />
+          <meta name="author" content="Sofia Johnsson S." />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
           <title>Sofia Johnsson S.</title>
         </Head>
         <body>
