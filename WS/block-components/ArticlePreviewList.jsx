7040da63e3312796812e7584fcc4e395894adf27
sofia johnsson <sofia.johnsson@willandskill.se>
2022-12-21 13:55:00 +0100

    changed article card styling, longer paper

--- WS/block-components/ArticlePreviewList.jsx
+++ WS/block-components/ArticlePreviewList.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 const renderArticleCard = (article) => {
   const { content } = article;
   return (
-    <div className="relative h-fit w-full lg:mt-20 " key={article.uuid}>
+    <div className="relative h-fit w-full lg:mt-16 " key={article.uuid}>
       <div className="w-full h-72">
         <div
           style={{ backgroundImage: `url(${content?.image?.filename})` }}
@@ -14,9 +14,11 @@ const renderArticleCard = (article) => {
         />
         <div className="absolute top-0 left-0 w-full h-72 bg-secondary-main opacity-40" />
       </div>
-      <div className="relative bottom-20 w-11/12 mx-auto bg-card-background bg-cover bg-top px-3 lg:px-5 pt-4 lg:pt-11 pb-3 lg:pb-7">
-        <Heading type="h3">{article?.name}</Heading>
-        <Text>{content?.summary}</Text>
+      <div className="flex flex-col justify-between relative bottom-16 w-11/12 min-h-[380px] mx-auto bg-card-background bg-cover bg-top px-3 lg:px-5 pt-4 lg:pt-11 pb-3 lg:pb-7">
+        <div>
+          <Heading type="h3">{article?.name}</Heading>
+          <Text>{content?.summary}</Text>
+        </div>
         <div className="flex my-3 ">
           <Link
             className="flex items-center h-fit ml-1 mt-5"
