5dc705a7e248026d08f815bcf4078c2f647d406f
sofia johnsson <sofia.johnsson@willandskill.se>
2022-12-21 16:19:52 +0100

    Changed avatar for Image

--- WS/block-components/Avatar.jsx
+++ WS/block-components/Avatar.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import Image from "next/image";
 
 const Avatar = ({ blok }) => {
   return (
     <div className="relative lg:bottom-16 flex md:justify-center self-center w-full h-96 lg:max-w-3xl lg:h-100 truncate">
       <div className="avatar_bg relative left-0 lg:top-32 flex justify-center items-center bg-secondary-main">
         <div
-          className={`h-fit w-fit absolute lg:${
+          className={`h-[90%] w-full absolute lg:${
             (blok?.y_axis > 0 ? "top-" : "bottom") + blok?.y_axis
           } lg:${(blok?.x_axis > 0 ? "right-" : "left") + blok?.x_axis}`}
         >
-          <img
-            className="max-h-[669px] max-w-[400px]"
+          <Image
+            fill
             src={blok?.avatar_image?.filename}
             alt={blok?.avatar_image?.alt}
           />
