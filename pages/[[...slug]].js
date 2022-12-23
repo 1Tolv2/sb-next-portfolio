0b85722431170dca96f4143b9a44cde76860de19
1tolv2 <sofiaq1@gmail.com>
2022-12-23 09:13:52 +0100

    broke out storyblokUtils to separate file

--- pages/[[...slug]].js
+++ pages/[[...slug]].js
@@ -1,13 +1,9 @@
 import { useState, createContext } from "react";
 import Head from "next/head";
-import {
-  getStoryblokApi,
-  useStoryblokState,
-  StoryblokComponent,
-} from "@storyblok/react";
+import { useStoryblokState, StoryblokComponent } from "@storyblok/react";
 import Layout from "../WS/Layout";
 import CoverModal from "../WS/components/CoverModal";
-import { generateSitemap } from "../WS/utils/sitemap";
+import { StoryblokUtils } from "../WS/utils/cms";
 
 const ModalContext = createContext();
 
@@ -43,82 +39,16 @@ export default function Page({ story, configData }) {
 export { ModalContext };
 
 export async function getStaticPaths() {
-  const storyblokApi = getStoryblokApi();
-
-  let { data } = await storyblokApi.get("cdn/links");
-  const { links } = data;
-  const hiddenSlugs = ["config"];
-
-  let paths = [];
-  Object.keys(links).forEach((linkKey) => {
-    const slug = data.links[linkKey].slug;
-    if (links[linkKey].is_folder || hiddenSlugs.includes(slug)) return;
-
-    if (slug === "home") {
-      paths.push({ params: { slug: [""] } });
-    } else if (/^projects\/(.+)/.test(slug)) {
-      return;
-    } else {
-      let splittedSlug = slug.split("/");
-      paths.push({ params: { slug: splittedSlug } });
-    }
-  });
-
-  generateSitemap(paths);
-  return {
-    paths,
-    fallback: false,
-  };
+  const data = await StoryblokUtils.getStaticPaths();
+  return data;
 }
 
 export async function getStaticProps({ params }) {
-  let slug = params.slug ? params.slug.join("/") : "home";
-
-  let sbParams = {
-    version: process.env.NODE_ENV === "development" ? "draft" : "published",
-    resolve_relations: [
-      "project-preview-list.projects",
-      "article-preview-list.articles",
-    ],
-  };
-
-  const storyblokApi = getStoryblokApi();
-  const { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
-  const configData = await storyblokApi.get(`cdn/stories`, {
-    ...sbParams,
-    starts_with: "config",
-  });
-
-  async function getContentData(slug) {
-    const { data } = await storyblokApi.get(`cdn/stories`, {
-      ...sbParams,
-      starts_with: slug,
-      is_startpage: 0,
-    });
-    return data;
+  if (!params) {
+    params = {
+      slug: ["home"],
+    };
   }
-  if (
-    (slug === "projects" || slug === "articles") &&
-    data?.story?.content?.body
-      ?.find((item) => item?.component === "titled-grid")
-      ?.body?.find(
-        (item) =>
-          item?.component === `${slug.substr(0, slug.length - 1)}-preview-list`
-      )?.[slug]?.length === 0
-  ) {
-    const contentData = await getContentData(slug);
-    data.story.content.body
-      .find((item) => item.component === "titled-grid")
-      .body.find(
-        (item) =>
-          item.component === `${slug.substr(0, slug.length - 1)}-preview-list`
-      )[slug] = contentData ? contentData?.stories : [];
-  }
-  return {
-    props: {
-      story: data ? data.story : false,
-      configData: configData ? configData.data.stories[0] : false,
-      key: data ? data.story.id : false,
-    },
-  };
+  const data = await StoryblokUtils.getStaticProps(params);
+  return data;
 }
