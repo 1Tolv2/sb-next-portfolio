d56e55dc471d5e53a4771af10512ce69e9935dcf
1tolv2 <sofiaq1@gmail.com>
2022-12-23 09:30:03 +0100

    adjusted projectList, there can be less projects

--- WS/block-components/ProjectPreviewList.jsx
+++ WS/block-components/ProjectPreviewList.jsx
@@ -4,7 +4,7 @@ import ProjectMobileCard from "../components/ProjectCards/ProjectMobileCard";
 
 const ProjectPreviewList = ({ blok }) => {
   return (
-    <div className="grid lg:grid-cols-3 lg:grid-rows-1 w-3/4 lg:w-auto mx-auto gap-14 items-end">
+    <div className="flex flex-col justify-evenly lg:flex-row w-3/4 lg:w-auto mx-auto gap-14 items-end">
       {blok?.projects?.map((project) => {
         return project?.content?.platform === "mobile" ? (
           <ProjectMobileCard story={project} key={project?.uuid} />
