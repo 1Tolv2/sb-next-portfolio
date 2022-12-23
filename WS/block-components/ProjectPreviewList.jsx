2d3908ddfa11973f240ec8b45b8892c19cd359ea
1tolv2 <sofiaq1@gmail.com>
2022-12-23 10:55:13 +0100

    style fixes

--- WS/block-components/ProjectPreviewList.jsx
+++ WS/block-components/ProjectPreviewList.jsx
@@ -3,13 +3,29 @@ import ProjectDesktopCard from "../components/ProjectCards/ProjectDesktopCard";
 import ProjectMobileCard from "../components/ProjectCards/ProjectMobileCard";
 
 const ProjectPreviewList = ({ blok }) => {
+  const link_texts = {
+    source_code: blok?.source_code_link_text,
+    deploy: blok?.deploy_link_text,
+  };
   return (
     <div className="flex flex-col justify-evenly lg:flex-row w-3/4 lg:w-auto mx-auto gap-14 items-end">
       {blok?.projects?.map((project) => {
         return project?.content?.platform === "mobile" ? (
-          <ProjectMobileCard story={project} key={project?.uuid} />
+          <ProjectMobileCard
+            story={{
+              project,
+              link_texts,
+            }}
+            key={project?.uuid}
+          />
         ) : (
-          <ProjectDesktopCard story={project} key={project?.uuid} />
+          <ProjectDesktopCard
+            story={{
+              project,
+              link_texts,
+            }}
+            key={project?.uuid}
+          />
         );
       })}
     </div>
